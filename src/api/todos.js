01bfc33f30121d58d273a75b717393d41202853c
melody <vivihl2131@gmail.com>
2023-05-24 01:35:34 +0800

    add update todo and delete todo api

--- src/api/todos.js
+++ src/api/todos.js
@@ -26,6 +26,25 @@ export const createTodo = async (payload) => {
   }
 };
 
-export const patchTodo = () => {};
+export const patchTodo = async (payload) => {
+  const { id, title, isDone } = payload;
+  try {
+    const res = await axios.patch(`${baseUrl}/todos/${id}`, {
+      title,
+      isDone,
+    });
 
-export const deleteTodo = () => {};
+    return res.data;
+  } catch (error) {
+    console.error('[Patch Todo failed]:', error);
+  }
+};
+
+export const deleteTodo = async (id) => {
+  try {
+    const res = await axios.delete(`${baseUrl}/todos/${id}`);
+    return res.data;
+  } catch (error) {
+    console.error('[Delete Todo failed]:', error);
+  }
+};
