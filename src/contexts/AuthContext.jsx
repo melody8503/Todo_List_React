1f9ba997963cba8724b5b5771e56e0051823ee0b
melody <vivihl2131@gmail.com>
2023-05-26 18:30:17 +0800

    add auth function to context

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,6 @@
 import { createContext, useState } from 'react';
+import { login, register } from 'api/auth';
+import * as jwt from 'jsonwebtoken';
 
 // 初始狀態與方法
 const defaultAuthContext = {
@@ -25,8 +27,54 @@ const AuthProvider = ({ children }) => {
     <AuthContext.Provider
       value={{
         isAuthenticated,
-        currentMember: payload,
+        currentMember: payload && {
+          //取得登入的使用者資訊
+          id: payload.sub,
+          name: payload.name,
+        },
+        register: async (data) => {
+          const { success, authToken } = await register({
+            username: data.username,
+            email: data.email,
+            password: data.password,
+          });
+
+          // 取得 payload 內容
+          const tempPayload = jwt.decode(authToken);
+          if (tempPayload) {
+            setPayload(tempPayload);
+            setIsAuthenticated(true);
+            localStorage.setItem('authToken', authToken);
+          } else {
+            setPayload(null);
+            setIsAuthenticated(false);
+          }
+          return success;
+        },
+        login: async (data) => {
+          const { success, authToken } = await login({
+            username: data.username,
+            password: data.password,
+          });
+          const tempPayload = jwt.decode(authToken);
+          if (tempPayload) {
+            setPayload(tempPayload);
+            setIsAuthenticated(true);
+            localStorage.setItem('authToken', authToken);
+          } else {
+            setPayload(null);
+            setIsAuthenticated(false);
+          }
+          return success;
+        },
+        logout: () => {
+          localStorage.removeItem('authToken');
+          setPayload(null);
+          setIsAuthenticated(false);
+        },
       }}
-    ></AuthContext.Provider>
+    >
+      {children}
+    </AuthContext.Provider>
   );
 };
